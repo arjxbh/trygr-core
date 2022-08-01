7343ede61ea8a006e46262dca6c73436ee78b879
Arjun <arjxbh@gmail.com>
2022-07-31 22:51:45 -0400

    remove refresh token from committed code

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,1 +1,2 @@
 // environment config goes here
+module.exports = { RING_REFRESH_TOKEN: '' };
--- ring.js
+++ ring.js
@@ -1,8 +1,9 @@
 const { RingApi } = require('ring-client-api');
+const { RING_REFRESH_TOKEN } = require('./app/config/env');
 
 const doit = async () => {
     const ringApi = new RingApi({
-        refreshToken: 'eyJhbGciOiJIUzUxMiIsImprdSI6Ii9vYXV0aC9pbnRlcm5hbC9qd2tzIiwia2lkIjoiYzEyODEwMGIiLCJ0eXAiOiJKV1QifQ.eyJpYXQiOjE2NTkzMDA3NzgsImlzcyI6IlJpbmdPYXV0aFNlcnZpY2UtcHJvZDp1cy1lYXN0LTE6ZjEwMmRlOWIiLCJyZWZyZXNoX2NpZCI6InJpbmdfb2ZmaWNpYWxfYW5kcm9pZCIsInJlZnJlc2hfc2NvcGVzIjpbImNsaWVudCJdLCJyZWZyZXNoX3VzZXJfaWQiOjYxODYyMDAxLCJybmQiOiItTlpCUnlJYTNrMS1jUSIsInNlc3Npb25faWQiOiI0YjViNDFmZC0zMmE2LTRiYjQtOTJkNS00MGFhYjZhZWU4MjEiLCJ0eXBlIjoicmVmcmVzaC10b2tlbiJ9.Rn_KSjtUlK3RUOXYc-NPqm_52VaiTyk-zW2jgLRJMKY9oFWLGY8F_i4Xg4xBWPOPIMdLE5YhCyPkzW8RCoS5cw'
+        refreshToken: RING_REFRESH_TOKEN
       });
       
       const locations = await ringApi.getLocations()
