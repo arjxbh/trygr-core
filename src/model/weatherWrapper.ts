6f78075e9b75db0867e70a92b8a96f33773003fe
Arjun Bhatnagar <arjxbh@gmail.com>
2022-11-25 22:02:16 -0500

    added some comments for weather and zip code lookup

---
+++ src/model/weatherWrapper.ts
@@ -0,0 +1,5 @@
+/***
+ * https://open-meteo.com/en/docs#latitude=40.36&longitude=-74.24&hourly=temperature_2m,temperature_120m&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York
+ * 
+ * https://api.open-meteo.com/v1/forecast?latitude=40.36&longitude=-74.24&hourly=temperature_2m,temperature_120m&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York
+ */
---
+++ src/model/zipCodeWrapper.ts
@@ -0,0 +1,4 @@
+/***
+ * curl "https://app.zipcodebase.com/api/v1/search?codes=10005,10006" \
+    -H "apikey: {apikey}"
+ */
